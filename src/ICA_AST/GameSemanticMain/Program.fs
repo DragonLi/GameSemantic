7474e990d98b24086a59599721735525bfc298f4
Marina <polubelovam@gmail.com>
2016-08-25 01:24:19 +0300

    add tests

--- src/ICA_AST/GameSemanticMain/Program.fs
+++ src/ICA_AST/GameSemanticMain/Program.fs
@@ -4,15 +4,7 @@ open GS.ICA.AST
 
 [<EntryPoint>]
 let main argv = 
-    //let ast = GS.ICA.Pars.Main.parse "ss(skip; skip) + skip; !skip + fix \x.(m || n + 8)"
-    //let ast = GS.ICA.Pars.Main.parse "(\ x . x (2) + 8)(\y.y + y)"
-    //let ast = GS.ICA.Pars.Main.parse "(\ x . x (2) + (x (8)))(\y.y + y)"
-    //let ast = GS.ICA.Pars.Main.parse "new x in !x + 1"
-    //let ast = GS.ICA.Pars.Main.parse "(\ f . new x in (f(x) + (f(x))))(\ x . !x + 1)"
-    //let ast = GS.ICA.Pars.Main.parse "(\ g . (\ f . new x in (f(x) + (g(x))))(\ a . a := (!a + 1)))(\ b . b := (!b + 2))"
-    //let ast = GS.ICA.Pars.Main.parse "(\ g . (\ f . new x in ((g(x))+(!(f(x))) + 1))(\ a . a ))(\ b . b := (!b + 2))"
-    //let ast = GS.ICA.Pars.Main.parse "new x in ((x := 2);(x := (!x + 1));(!x))"
-    let ast = GS.ICA.Pars.Main.parse "new x in ((x := 2); if !x then x := (!x + 1) else x := (!x + 3))"
+    let ast = GS.ICA.Pars.Main.parse "new x in new y in ((x := 2); (y := (!x + 3)); if !x then x := (!x + 1) else x := (!y + !x + 3))"
     printfn "%A" ast
     let t = GS.ICA.AST.translate ast.[0] []
     let r = 
